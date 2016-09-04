e70ea1ac046c7ff42539aa224c252c01a63636cd
TobiahRex <tobiah.rex@gmail.com>
2016-09-04 02:17:08 -0700

    completed AcapellaApi.js

---
+++ App/Services/AcapellaApi.js
@@ -0,0 +1,26 @@
+import { create } from 'apisauce'
+
+const create = (baseURL = 'http://vaas.acapella-group.com') => {
+
+  const api = create({ baseURL })
+
+  const getAudioMsg = (msg, msgVoice) => {
+    api.setHeaders({
+      prot_vers: 2,
+      cl_login: 'EVAL_VAAS',
+      cl_app: 'EVAL_6615982',
+      cl_pwd: 'ytjsg3bj',
+      req_voice: `${msgVoice}`,
+      req_text: `${msg}`
+    })
+    return api.get('/Services/UrlMaker?jsoncallback=?')
+  }
+
+  return {
+    getAudioMsg
+  }
+}
+
+export default {
+  create
+}
